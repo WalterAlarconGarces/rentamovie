063d53661fa22aba97a4f0433ca3029847acf874
Walter Alarcon Garces <walarcon@gmail.com>
2023-08-10 02:51:15 -0400

    creacion de seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+
+10.times do
+    @clients = Client.create( name: Faker::Name.name, age: Faker::Number.between(from: 18, to: 100), movie_id: Faker::Number.between(from: 1, to: 200))
+    puts @clients
+end
+100.times do
+    @movies = Movie.create( name: Faker::Movie.title)
+    puts @movies
+end
